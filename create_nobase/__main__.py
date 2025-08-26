ef55abbc08be5406c9a68f139b1adc1fa8e70c2a
tony-darco <tonydarco40@gmail.com>
2025-08-26 06:29:27 -0400

    init

---
+++ create_nobase/__main__.py
@@ -0,0 +1,17 @@
+
+if __name__ == "__main__":
+    print("creating nobase:")
+
+    #pull files about the api
+
+    #init verb creation
+        #pull api link
+        #api description
+        #api variables
+        #api attributes
+    
+    #create communities
+
+    #store
+
+    print("Done with nobase")
