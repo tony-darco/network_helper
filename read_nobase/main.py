d1f51339bf022db1f9927d37346b49b35690e5ac
tony-darco <tonydarco40@gmail.com>
2025-08-26 21:59:53 -0400

    Refactor main.py to implement read_graph function and update entry point

--- read_nobase/main.py
+++ read_nobase/main.py
@@ -1,13 +1,46 @@
 import asyncio
+from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+import pandas as pd
+
+import graphrag.api as api
+from graphrag.config.load_config import load_config
+
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
-
 from langchain_core.documents import Document
 
+PROJECT_DIRECTORY = "/Users/tdarco/Documents/Projects/network_helper/"
+
 load_dotenv()
+
+async def read_graph(query: str) -> str:
+    graphrag_config = load_config(Path(PROJECT_DIRECTORY))
+
+    entities = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/entities.parquet")
+    communities = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/communities.parquet")
+    community_reports = pd.read_parquet(
+        f"{PROJECT_DIRECTORY}/output/community_reports.parquet"
+    )
+    relationships = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/relationships.parquet")
+    text_units = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/text_units.parquet")
+
+    response, context = await api.drift_search(
+        config=graphrag_config,
+        entities=entities,
+        communities=communities,
+        community_reports=community_reports,
+        text_units=text_units,
+        relationships=relationships,
+        community_level=2,
+        response_type="url parameters and example",
+        query=query,
+    )
+
+    return(response)
+
 
 async def read_VecDB(query: str) -> list[Document]:
 
@@ -25,4 +58,4 @@ async def read_VecDB(query: str) -> list[Document]:
 
 if __name__ == "__main__":
     print("Reading No Base")
-    asyncio.run(read_VecDB("what api do i use to create an ssid"))
+    asyncio.run(read_graph("what api do i use to create an ssid"))
